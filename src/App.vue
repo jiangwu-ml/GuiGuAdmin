3bb1fa53079adf1a0463bce20701113ca0cef003
li.meng <michelle.meng@factchina.com>
2024-07-04 10:31:59 +0800

    INIT

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="box">
+    <h1>TEST</h1>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped lang="scss">
+.box {
+  width: 500px;
+  background-color: pink;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './style.css'
+
+createApp(App).mount('#app')
+
+var a = 100
+
+const arr = '1000'
